dd0747ec349944007d95b61888fde581086303de
Isaac Wong <339@holbertonschool.com>
2018-01-19 02:48:48 +0000

    Used proper boilerplate code

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,11 +15,15 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (abs(n % 10) < 6 && !abs(n % 10) == 0)
+	{
 		printf(
 "Last digit of %d is %d and is less than 6 and not 0\n", n, abs(n % 10));
+	}
 	else if (abs(n % 10) > 5 && !abs(n % 10) == 0)
+	{
 		printf(
 "Last digit of %d is %d and is greater than 5\n", n, abs(n % 10));
+	}
 	else
 		printf("Last digit of %d is %d and is 0\n", n, abs(n % 10));
 	return (0);
