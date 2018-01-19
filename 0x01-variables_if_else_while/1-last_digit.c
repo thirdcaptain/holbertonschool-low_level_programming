a6f03f4f9c0e6fc73742177a7d542942ede92374
Isaac Wong <339@holbertonschool.com>
2018-01-19 02:53:07 +0000

    Removed all abs()

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	if ((n % 10) < 6 && !(n % 10) == 0)
 	{
 		printf(
-"Last digit of %d is %d and is less than 6 and not 0\n", n, abs(n % 10));
+"Last digit of %d is %d and is less than 6 and not 0\n", n, (n % 10));
 	}
 	else if ((n % 10) > 5 && !(n % 10) == 0)
 	{
