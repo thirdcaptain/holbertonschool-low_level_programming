f5ae547e95ac32bdccce95c2f39e1d69b62016ce
Isaac Wong <339@holbertonschool.com>
2018-01-19 00:24:06 +0000

    Initial commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  *main - determine if a randomly generated number is positive or negative
+ *
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -14,8 +15,8 @@ int main(void)
 	if (n < 0)
 		printf("%d is negative\n", n);
 	else if (n > 0)
-		printf("%d is positive \n", n);
+	printf("%d is positive\n", n);
 	else
-		printf("%d is zero \n", n);
+		printf("%d is zero\n", n);
 	return (0);
 }
