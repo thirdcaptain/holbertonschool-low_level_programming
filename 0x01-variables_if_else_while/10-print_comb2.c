518db89055f8b9e0cc8b5d3d9f683dce4ae7b23d
Isaac Wong <339@holbertonschool.com>
2018-01-19 06:35:55 +0000

    Initial commit

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - 0, 1, ... 8, 9 using putchar and no var type char
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+	int num2;
+
+	for (num = 48; num <= 57; num++)
+	{
+		for (num2 = 48; num2 <= 57; num2++)
+		{
+			putchar(num);
+			putchar(num2);
+			if (!(num == 57 && num2 == 57))
+			{
+				putchar(',');
+				putchar(' ');
+			};
+		};
+	};
+
+	putchar('\n');
+	return (0);
+}
