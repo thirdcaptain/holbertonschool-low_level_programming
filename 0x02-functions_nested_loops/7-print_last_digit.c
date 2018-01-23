a7b5d4137f246f5898e157174da1322a9008b5c6
Isaac Wong <339@holbertonschool.com>
2018-01-23 18:12:51 +0000

    Declared variable

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,9 +9,11 @@
 
 int print_last_digit(int a)
 {
+	int last;
+
 	if (a < 0)
 		a = a * (-1);
-	a = a % 10;
-	_putchar(a + '0');
-	return (a);
+	last = a % 10;
+	_putchar(last + '0');
+	return (last);
 }
