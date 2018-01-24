6686853e8060d681457c7bae477183d49972d6e6
Isaac Wong <339@holbertonschool.com>
2018-01-24 03:46:36 +0000

    Initial commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ *add - adds two numbers and returns results.
+ *
+ *Return: Result of two numbers.
+ */
+
+int add(int a, int b)
+{
+	int result = a + b;
+	return (result);
+}
