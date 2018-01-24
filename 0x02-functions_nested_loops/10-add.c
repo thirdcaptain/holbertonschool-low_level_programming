cf1f11531fd9f75db6deda6c9933d05ed24f16ae
Isaac Wong <339@holbertonschool.com>
2018-01-24 03:49:15 +0000

    Fixed betty style

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,16 @@
 #include "holberton.h"
 
 /**
- *add - adds two numbers and returns results.
+ * add - adds two numbers and returns results.
+ * @a: First operand
+ * @b: Second operand
  *
- *Return: Result of two numbers.
+ * Return: Result of two numbers.
  */
 
 int add(int a, int b)
 {
 	int result = a + b;
+
 	return (result);
 }
