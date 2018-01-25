ece71aee2f5612e06e4b698c8d779b923d2e961f
Isaac Wong <isaac@thirdcaptain.com>
2018-01-25 02:01:40 +0000

    Initial commit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print natural numbers below 1024
+ */
+
+int main(void)
+{
+	int a;
+	int b = 0;
+	int sum = 0;
+
+	for (a = 0; a < 1024; a++)
+	{
+		if (a % 3 == 0)
+			sum = sum + a;
+		if (a % 5 == 0)
+			sum = sum + a;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
