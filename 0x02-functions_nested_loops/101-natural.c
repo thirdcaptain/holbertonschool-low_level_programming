ac68d89d0a4d5d5a4b8b69cf8ebd56ec8ca90e67
Isaac Wong <isaac@thirdcaptain.com>
2018-01-25 02:11:49 +0000

    Betty style

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,21 +5,18 @@
  *
  *Return: 0 Always success.
  */
-
 int main(void)
 {
 	int a;
 	int sum = 0;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0; a < 1024; a++)
 	{
 		if (a % 3 == 0)
 			sum = sum + a;
 		if (a % 5 == 0)
 			sum = sum + a;
 	}
-
 	printf("%d\n", sum);
-
 	return (0);
 }
