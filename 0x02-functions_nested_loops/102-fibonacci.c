3dd0771f74f4e8809ef34b5bfcb09d1b04899fe4
Isaac Wong <isaac@thirdcaptain.com>
2018-01-25 07:45:29 +0000

    Used long int

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,13 +2,14 @@
 
 /**
  *main - print the first 50 fibonacci numbers
+ *Return: 0 Always success.
  */
 
 int main(void)
 {
-	long long int a = 0;
-	long long int b = 1;
-	long long int c;
+	long int a = 0;
+	long int b = 1;
+	long int c;
 	int i;
 
 	for (i = 0; i < 50; i++)
@@ -18,10 +19,10 @@ int main(void)
 		b = c;
 		if (i < 49)
 		{
-		printf("%lld, ", c);
+		printf("%ld, ", c);
 		}
 		else
-			printf("%lld\n", c);
+			printf("%ld\n", c);
 	}
 	return (0);
 }
