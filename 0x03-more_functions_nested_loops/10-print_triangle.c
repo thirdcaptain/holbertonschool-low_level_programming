dc8a39f217ab3c687ace5eae302ddd3bdd8e38be
Isaac Wong <isaac@thirdcaptain.com>
2018-01-26 07:37:47 +0000

    Initial commit

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ *print_triangle - prints triangle of size, size.
+ *@size: Size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int row;
+	int space;
+	int hash;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (space = 1; space <= (size - row); space++)
+			{
+				_putchar(' ');
+			}
+			for (hash = 1; hash <= row; hash++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
