a0f55dc249fbb8d50e5e100d0e34f1f5a8799996
Isaac Wong <isaac@thirdcaptain.com>
2018-01-26 04:10:44 +0000

    Fixed newline requirements

--- 0x03-more_functions_nested_loops/6-print_line.c
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -10,9 +10,14 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	if (n == 0)
+		_putchar('\n');
+	else
 	{
-		_putchar('_');
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
