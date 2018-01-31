5a13b80acd57fce9f57281e50c03b8bf454de92f
Isaac Wong <isaac@thirdcaptain.com>
2018-01-31 07:09:16 +0000

    Initial commit

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array
+ * @a: Array to be printed
+ * @n: Number of elements in the array to be printed
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+			printf("%d\n", a[i]);
+	}
+}
