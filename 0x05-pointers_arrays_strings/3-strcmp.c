e4fda577cecec615f298d2dd3a24da7633ca14b3
Isaac Wong <isaac@thirdcaptain.com>
2018-02-02 06:02:33 +0000

    Initial commit

---
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - Compares two strings.
+ * @s1: First string to be compared.
+ * @s2: Second string to be compared.
+ *
+ *Return: Negative int if s1 < s2, 0 if s1 = s2, and positive int if s1 > s2.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int s1_size = 0;
+	int s2_size = 0;
+	int i = 0;
+	int j = 0;
+
+	while (s1[i])
+	{
+		s1_size += s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		s2_size += s2[j];
+		j++;
+	}
+	return (s1 - s2);
+}
