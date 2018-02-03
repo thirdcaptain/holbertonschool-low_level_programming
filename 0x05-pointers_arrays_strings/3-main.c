4ff6f459fc6d9f431261d243aac82f38cbfa3d87
Isaac Wong <isaac@thirdcaptain.com>
2018-02-03 03:15:59 +0000

    Adjusted difference calculation to meet requirements

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -15,9 +15,5 @@ int main(void)
 	printf("%d\n", _strcmp(s1, s2));
 	printf("%d\n", _strcmp(s2, s1));
 	printf("%d\n", _strcmp(s1, s1));
-
-	printf("%d\n", strcmp(s1, s2));
-	printf("%d\n", strcmp(s2, s1));
-	printf("%d\n", strcmp(s1, s1));
 	return (0);
 }
--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -5,19 +5,19 @@
  * @s1: First string to be compared.
  * @s2: Second string to be compared.
  *
- *Return: Negative int if s1 < s2, 0 if s1 = s2, and positive int if s1 > s2.
+ * Return: ASCII difference if s1 < s2 or s1 > s2, and 0 if s1 = s2.
  */
 
 int _strcmp(char *s1, char *s2)
 {
-/*	int cmp = 0;
+
 	int i = 0;
 
 	while (s1[i])
 	{
-		cmp += s1[i] - s2[i];
+		if (s1[i] < s2[i] || s1[i] > s2[i])
+			return (s1[i] - s2[i]);
 		i++;
 	}
-*/
-	return (s1 - s2);
+	return (0);
 }
