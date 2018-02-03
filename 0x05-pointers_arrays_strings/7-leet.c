9e0478b3176cabfb818f96267576a6fa6279d475
Isaac Wong <isaac@thirdcaptain.com>
2018-02-03 04:25:51 +0000

    Initial commit

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -1,6 +1,29 @@
 #include "holberton.h"
 
-char *leet(char *)
-{
+/**
+ * leet - Encodes a string into 1337.
+ * @s: String to be encoded.
+ *
+ * Return: Encoded string.
+ */
 
+char *leet(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char *spec = "aeotlAEOTL";
+	char *repl = "4307143071";
+
+	while (s[i])
+	{
+		while (spec[j])
+		{
+			if (s[i] == spec[j])
+				s[i] = repl[j];
+			j++;
+		}
+		i++;
+		j = 0;
+	}
+	return (s);
 }
