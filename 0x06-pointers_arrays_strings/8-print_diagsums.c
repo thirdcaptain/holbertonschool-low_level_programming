df6ff44bcc8ef955c00f37f2d4593964bbe83201
Isaac Wong <isaac@thirdcaptain.com>
2018-02-06 07:48:40 +0000

    Used copy of array

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -14,16 +14,17 @@ void print_diagsums(int *a, int size)
 	int sum_a = *a;
 	int sum_b = 0;
 	int max = size * size;
+	int *copy = a;
 
 	for (i = 0; i < max; i = i + size)
 	{
 		sum_a += *a;
 		a += size + 1;
 	}
-	for (j = max; j >= 0; j = j - size)
+	for (j = 0; j < max; j = j + size)
 	{
-		a -= size - 1;
-		sum_b += *a;
+		copy += size - 1;
+		sum_b += *copy;
 	}
 	printf("%d, %d\n", sum_a, sum_b);
 }
