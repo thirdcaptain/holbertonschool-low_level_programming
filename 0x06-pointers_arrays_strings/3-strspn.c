6dc9d589510deef9cd9c0938a98bca5ec7d2bd2f
Isaac Wong <isaac@thirdcaptain.com>
2018-02-06 03:47:46 +0000

    Initial commit

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -1,3 +1,33 @@
 #include "holberton.h"
 
 /**
+ * _strspn - get length of prefix substring
+ * @s: String to be evaluated.
+ * @accept: String of values to accept.
+ *
+ * Return: The number of bytes in accept which are in s.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int count = 0;
+	int i;
+
+	while (*s)
+	{
+		i = 0;
+		while (accept[i])
+		{
+			if (*s == accept[i])
+			{
+				count++;
+				break;
+			}
+			i++;
+			if (accept[i] == '\0')
+				return (count);
+		}
+		s++;
+	}
+	return (count);
+}
