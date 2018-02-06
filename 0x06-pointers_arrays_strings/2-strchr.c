7f0ea2f15e2cb555297dc9766c6590d13f97ad96
Isaac Wong <isaac@thirdcaptain.com>
2018-02-06 03:25:22 +0000

    Accounted for \0

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#define NULL 0
 
 /**
  * _strchr - locates a character in a string
@@ -17,5 +18,7 @@ char *_strchr(char *s, char c)
 			return (s);
 		s++;
 	}
-	return (0);
+	if (c == '\0')
+		return (s);
+	return (NULL);
 }
