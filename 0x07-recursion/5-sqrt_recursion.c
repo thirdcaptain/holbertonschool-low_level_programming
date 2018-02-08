d36391045c30ca5e383c197a7bfe33c1d7da83a9
Isaac Wong <isaac@thirdcaptain.com>
2018-02-08 06:41:00 +0000

    Initial commit

---
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - returns natural square root of a number
+ * @n: Number to determine square root.
+ *
+ * Return: Square root of a number.
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (-1);
+	return (0);
+}
