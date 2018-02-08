48610d3d27fcc8d57240f3255e690696a71fd9f2
Isaac Wong <isaac@thirdcaptain.com>
2018-02-08 07:47:38 +0000

    Initial commit

---
+++ 0x07-recursion/6-is_prime_number.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - returns 1 if input integer is a prime number
+ * @n: Number to determine if prime.
+ *
+ * Return: 1 if n is prime, 0 otherwise.
+ */
+
+int is_prime_number(int n)
+{
+	if (n > 0)
+		return (1);
+	return (0);
+}
---
+++ 0x07-recursion/7-is_palindrome.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * is_palindrome - determines if a string is a palindrome
+ * @s: String to evaluate.
+ *
+ * Return: 1 if a string is a palindrome, 0 otherwise.
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (0);
+}
