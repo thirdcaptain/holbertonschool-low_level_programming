50ba2c688136bbc81a1f5c323628312b58efa1db
Isaac Wong <isaac@thirdcaptain.com>
2018-02-09 07:41:25 +0000

    Adjust logic

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -1,6 +1,23 @@
 #include "holberton.h"
 
 /**
+ * calc_prime - calculates if a number is prime
+ * @i: number to calculate prime
+ * @n: Number to determine if prime
+ *
+ * Return: 1 if n is prime, 0 otherwise.
+ */
+
+int calc_prime(int i, int num)
+{
+	if (i < 2)
+		return (1);
+	if (num % i == 0)
+		return (0);
+	return (calc_prime((i - 1), num));
+}
+
+/**
  * is_prime_number - returns 1 if input integer is a prime number
  * @n: Number to determine if prime.
  *
@@ -9,7 +26,9 @@
 
 int is_prime_number(int n)
 {
-	if (n > 0)
-		return (1);
-	return (0);
+	int calc = n - 1;
+
+	if (n <= 1)
+		return (0);
+	return (calc_prime(calc, n));
 }
