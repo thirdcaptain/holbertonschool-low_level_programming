58506fec704b55b72fdc40b1946d0088cadd33c4
Isaac Wong <isaac@thirdcaptain.com>
2018-02-14 20:08:51 +0000

    Adjusted free for loop

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
-			for (i = i - 1; i >= 0; i++)
+			for (i = i - 1; i >= 0; i--)
 			{
 				free(grid[i]);
 			}
