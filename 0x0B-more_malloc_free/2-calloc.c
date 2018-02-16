0ed6488052a8965f0c3d2ce6b70832f3973cd263
Isaac Wong <isaac@thirdcaptain.com>
2018-02-16 03:07:07 +0000

    Added type-cast

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -12,13 +12,19 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-
 	void *pointer;
+	unsigned int i;
 
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
+
 	pointer = malloc(nmemb * size);
 	if (pointer == NULL)
 		return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+	{
+		*((char *)pointer + i) = 0;
+	}
 	return (pointer);
 }
