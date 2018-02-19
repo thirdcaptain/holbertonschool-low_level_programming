8002b6757c18d6b4e046c3e13dabf921c385b743
Isaac Wong <isaac@thirdcaptain.com>
2018-02-19 23:38:53 +0000

    Initial commit

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - print name of file it was compiled from
+ *
+ * Return: 0 - always success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+}
