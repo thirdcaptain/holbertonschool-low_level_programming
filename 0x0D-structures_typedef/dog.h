4505905f7e39778e13cf4f54e92241c267207ff6
Isaac Wong <isaac@thirdcaptain.com>
2018-02-20 03:38:36 +0000

    Added struct definition

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,5 +1,15 @@
 #ifndef DOG
 #define DOG
+
+/**
+ * struct dog - Dog characteristics
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ *
+ * Description: Characteristics of dog
+ */
+
 struct dog
 {
 	char *name;
