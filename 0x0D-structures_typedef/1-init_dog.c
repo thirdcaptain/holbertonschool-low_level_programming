9b6f43cd147d4a660558fcbc35efa9ab1cca7067
Isaac Wong <isaac@thirdcaptain.com>
2018-02-20 04:48:12 +0000

    Initial commit

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes struct dog with data
+ * @d: struct passed to function
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner to initialize
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
