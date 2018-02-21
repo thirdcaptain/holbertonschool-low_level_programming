adf3413038a6b733cb39382a6832fc28ccdd0b4f
Isaac Wong <isaac@thirdcaptain.com>
2018-02-21 23:10:48 +0000

    Adjusted comments

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
-* print_name - prints a name
-* @name: name to print
-* @f: function to call to print
-*/
+ * print_name - prints a name
+ * @name: name to print
+ * @f: function to call to print
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
