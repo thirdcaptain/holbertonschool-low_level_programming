add931bf42e33d3743375b2aebf444c38ba3bf07
Isaac Wong <isaac@thirdcaptain.com>
2018-02-21 22:27:05 +0000

    Initial commit

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: function to call to print
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
