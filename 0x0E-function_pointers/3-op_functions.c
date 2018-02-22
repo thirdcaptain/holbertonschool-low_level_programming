4535fcca0af1505f715fb86cab787d99b0923e89
Isaac Wong <isaac@thirdcaptain.com>
2018-02-22 07:46:20 +0000

    Initial commit

---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,68 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of integer parameters
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference of integer parameters
+ */
+
+
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product of integer parameters
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: quotient of integer parameters
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder of integer parameters
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
