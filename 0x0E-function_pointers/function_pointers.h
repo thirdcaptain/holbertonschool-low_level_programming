73e73b715901524f19803e5a16224f4b9142861e
Isaac Wong <isaac@thirdcaptain.com>
2018-02-24 07:45:11 +0000

    Initial commit

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,6 +1,21 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 #include <stdio.h>
+
+/**
+ * struct data_type - struct for character type
+ *
+ * @type: the type selector
+ * @f: The function associated
+ */
+typedef struct data_type
+{
+	char *data_type;
+	void (*f)(char *string);
+} data_type_t;
+
+
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
