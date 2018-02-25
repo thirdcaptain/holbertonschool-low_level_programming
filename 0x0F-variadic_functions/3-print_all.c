b312ed49e97c99a0406cf13d844093bdad2b8ebe
Isaac Wong <isaac@thirdcaptain.com>
2018-02-25 06:57:54 +0000

    Fixed NULL checks

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -2,49 +2,48 @@
 
 /**
  * print_c - prints characters
- * @argument - argument to print
+ * @argument: argument to print
  */
 
 void print_c(va_list argument)
 {
-	printf("%c",va_arg(argument, int));
+	printf("%c", va_arg(argument, int));
 }
 
 /**
  * print_i - prints integers
- * @argument - argument to print
+ * @argument: argument to print
  */
 
 void print_i(va_list argument)
 {
-	printf("%d",va_arg(argument, int));
+	printf("%d", va_arg(argument, int));
 }
 
 /**
  * print_f - prints floats
- * @argument - argument to print
+ * @argument: argument to print
  */
 
 void print_f(va_list argument)
 {
-	printf("%f",va_arg(argument, double));
+	printf("%f", va_arg(argument, double));
 }
 
 /**
  * print_s - prints strings
- * @argument - argument to print
+ * @argument: argument to print
  */
 
 void print_s(va_list argument)
 {
 	if (argument == NULL)
 		printf("(nil)");
-	printf("%s",va_arg(argument, char*));
+	printf("%s", va_arg(argument, char*));
 }
 
 /**
  * print_all - print all arguments of specified type
- *
  * @format: The format types which can be printed
  */
 
@@ -63,9 +62,8 @@ void print_all(const char * const format, ...)
 	char *s1 = ", ";
 
 	va_start(valist, format);
-	
 	i = 0;
-	while (format[i] != '\0' && format != NULL)
+	while (format != NULL && format[i] != '\0')
 	{
 		j = 0;
 		while (data[j].data_type != NULL)
