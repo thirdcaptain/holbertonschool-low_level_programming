77a92caa0aa5bef756ad60bd7c87f50e1cb5ed72
Isaac Wong <isaac@thirdcaptain.com>
2018-02-25 06:49:11 +0000

    Initial commit

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,84 @@
+#include "variadic_functions.h"
+
+/**
+ * print_c - prints characters
+ * @argument - argument to print
+ */
+
+void print_c(va_list argument)
+{
+	printf("%c",va_arg(argument, int));
+}
+
+/**
+ * print_i - prints integers
+ * @argument - argument to print
+ */
+
+void print_i(va_list argument)
+{
+	printf("%d",va_arg(argument, int));
+}
+
+/**
+ * print_f - prints floats
+ * @argument - argument to print
+ */
+
+void print_f(va_list argument)
+{
+	printf("%f",va_arg(argument, double));
+}
+
+/**
+ * print_s - prints strings
+ * @argument - argument to print
+ */
+
+void print_s(va_list argument)
+{
+	if (argument == NULL)
+		printf("(nil)");
+	printf("%s",va_arg(argument, char*));
+}
+
+/**
+ * print_all - print all arguments of specified type
+ *
+ * @format: The format types which can be printed
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list valist;
+	int i, j;
+	data_type_t data[] = {
+	{"c", print_c},
+	{"i", print_i},
+	{"f", print_f},
+	{"s", print_s},
+	{NULL, NULL}
+	};
+	char *s = "";
+	char *s1 = ", ";
+
+	va_start(valist, format);
+	
+	i = 0;
+	while (format[i] != '\0' && format != NULL)
+	{
+		j = 0;
+		while (data[j].data_type != NULL)
+		{
+			if (*(data[j].data_type) == format[i])
+			{
+				printf("%s", s);
+				data[j].f(valist);
+				s = s1;
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+}
--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -4,6 +4,24 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+/**
+ * struct data_type - struct for character type
+ *
+ * @type: the type selector
+ * @f: The function associated
+ */
+
+typedef struct data_type
+{
+        char *data_type;
+        void (*f)(va_list);
+} data_type_t;
+
+void print_c(va_list argument);
+void print_i(va_list argument);
+void print_f(va_list argument);
+void print_s(va_list argument);
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
