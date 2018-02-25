1b507d592a852210e64d1a225b85dba4a29a1c71
Isaac Wong <isaac@thirdcaptain.com>
2018-02-25 07:57:11 +0000

    Betty fixes

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -8,14 +8,14 @@
 /**
  * struct data_type - struct for character type
  *
- * @type: the type selector
+ * @data_type: the type selector
  * @f: The function associated
  */
 
 typedef struct data_type
 {
-        char *data_type;
-        void (*f)(va_list);
+	char *data_type;
+	void (*f)(va_list);
 } data_type_t;
 
 void print_c(va_list argument);
