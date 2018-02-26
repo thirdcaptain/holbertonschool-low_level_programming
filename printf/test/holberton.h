6cec810d1f7c288b0a20e13c8cef1082b13ea4c0
Isaac Wong <isaac@thirdcaptain.com>
2018-02-26 23:15:14 +0000

    Initial commit

---
+++ printf/test/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <unistd.h>
+
+int _printf(const char *format);
+int _strlen(const char *s);
+
+#endif
---
+++ printf/test/write.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main(void)
+{
+	char *string;
+	int str_len;
+
+	string = "strong\n";
+	str_len = 7;
+
+	write(1, "Test\n", 5);
+
+	write(1, string, str_len); 
+
+	return 0;
+}
