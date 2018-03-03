651f8b7a682d730ac5be8f391aaaa2e403e1a118
Isaac Wong <isaac@thirdcaptain.com>
2018-03-03 04:49:45 +0000

    Betty fixes

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  * before_main - prints text before main is executed
  */
 
-void before_main (void)
+void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
