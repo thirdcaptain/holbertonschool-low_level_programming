3646a9f9b4eaae31062bee65127b82d89bfa6c5e
Isaac Wong <isaac@thirdcaptain.com>
2018-03-06 19:55:57 +0000

    Set next value to NULL

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -16,7 +16,7 @@ void free_listint2(listint_t **head)
 	if (*head != NULL)
 	{
 		position = (*head)->next;
-/*		(*head)->next = NULL;*/
+		(*head)->next = NULL;
 		while (position != NULL)
 		{
 			temp = position->next;
