c7fd7c92a585d3334efc1ac2948260f1a37590de
Isaac Wong <isaac@thirdcaptain.com>
2018-03-07 01:00:11 +0000

    Operator removal

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 	listint_t *position, *node;
 
-	if (head == NULL || (*head) == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	node = malloc(sizeof(listint_t));
