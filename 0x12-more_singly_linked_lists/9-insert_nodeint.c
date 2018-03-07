cda063c8954548134f4cddd8757efdc8e4f5024f
Isaac Wong <isaac@thirdcaptain.com>
2018-03-07 00:26:13 +0000

    Added end condition

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -59,7 +59,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	next = *head;
 	for (i = 1; i < (idx + 1); i++)
 		next = next->next;
-	if (idx == (count - 1))
+	if (idx == count)
 	{
 		prev->next = node;
 		node->next = NULL;
