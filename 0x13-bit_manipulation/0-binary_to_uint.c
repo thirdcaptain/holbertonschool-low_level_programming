5c4225e5461a5a6fd418f5e538fb1e323e7e8fa0
Isaac Wong <isaac@thirdcaptain.com>
2018-03-09 18:29:13 +0000

    Used bit operators

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -1,66 +1,27 @@
 #include "holberton.h"
 
 /**
- * _strlen - gets length of a string
- * @s: string
- *
- * Return: length of string
- */
-
-unsigned int _strlen(const char *s)
-{
-	unsigned int length = 0;
-
-	while (*s != '\0')
-	{
-		s++;
-		length++;
-	}
-	return (length);
-}
-
-/**
- * two_exponent - returns power
- * @power: the power to raise base 2
- *
- * Return: result of power operation
- */
-
-unsigned int two_exponent(unsigned int power)
-{
-	unsigned int result = 1, i;
-
-	for (i = 0; i < power; i++)
-	{
-		result = result * 2;
-	}
-	return (result);
-}
-
-/**
  * binary_to_uint - convert a binary number to an unsigned int
- * @b: number to convert
+ * @b: string to convert
  *
- * Return: converted number
+ * Return: converted number, 0 if failed
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-	int count = 0, i, j;
-	unsigned int sum = 0;
+	unsigned int num = 0;
 
 	if (b == NULL)
 		return (0);
-	count = _strlen(b);
-	j = count - 1;
-	for (i = 0; i < count; i++)
-	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-		if (b[i] == '1')
-			sum += two_exponent(j);
-		j--;
-	}
-	return (sum);
-}
 
+	while (*b != '\0')
+	{
+		num = num << 1;
+		if (*b != '0' && *b != '1')
+			return (0);
+		if (*b == '1')
+			num = num ^ 1;
+		b++;
+	}
+	return (num);
+}
