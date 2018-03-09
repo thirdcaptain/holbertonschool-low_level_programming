f1b9297f39c0e5f383dcc589eeaea037b9494717
Isaac Wong <isaac@thirdcaptain.com>
2018-03-09 23:29:32 +0000

    Initial commit

---
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * set_bit - sets the value of a bit to 1
+ * @n: number to modify
+ * @index: index to set bit to 1 at
+ *
+ * Return: 1 on success, -1 on error
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int max_index;
+
+	max_index = sizeof(unsigned long int) * 8;
+	if (index > max_index)
+		return (-1);
+
+	*n = *n >> index;
+	*n = *n ^ 1;
+	*n = *n << index;
+	return (1);
+}
