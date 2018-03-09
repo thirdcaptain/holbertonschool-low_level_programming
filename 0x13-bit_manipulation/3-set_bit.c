d910a2f3280794e752535defa6ebc27327eabed0
Isaac Wong <isaac@thirdcaptain.com>
2018-03-09 23:54:16 +0000

    Used mask

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -11,19 +11,16 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int max_index;
+	unsigned int mask;
 
 	max_index = sizeof(unsigned long int) * 8;
 	if (index > max_index)
 		return (-1);
 
+	mask = 1 << index;
 	if (n == NULL)
 		return (-1);
 
-	if (*n == 0)
-		*n = *n ^ 1;
-
-	*n = *n >> index;
-	*n = *n ^ 1;
-	*n = *n << index;
+	*n = (*n) | mask;
 	return (1);
 }
