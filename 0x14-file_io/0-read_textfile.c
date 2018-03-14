ecd52f9616ce4ae59c67fe40034ecb369a06b4d3
Isaac Wong <isaac@thirdcaptain.com>
2018-03-14 05:08:26 +0000

    added error checks

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -13,7 +13,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	int fd;
 	char *buf;
-	ssize_t length;
+	ssize_t length, error_write;
+
+	if (filename == NULL)
+		return (0);
 
 	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
@@ -23,12 +26,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 	{
 		free(buf);
-		return(0);
+		return (0);
 	}
 
 	length = read(fd, buf, letters);
+	if (length == -1)
+		return (0);
 	buf[letters] = '\0';
-	write(STDOUT_FILENO, buf, letters);
+	error_write = write(STDOUT_FILENO, buf, letters);
+	if (error_write == -1)
+		return (0);
+	free(buf);
 	close(fd);
-	return(length);
+	return (length);
 }
