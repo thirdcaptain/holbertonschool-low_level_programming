33443fdd19aea76945864bf341fcc44f75c88c59
Isaac Wong <isaac@thirdcaptain.com>
2018-05-11 22:10:07 +0000

    Interated through linked list

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -12,11 +12,19 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index = 0;
+	hash_node_t *cursor;
 
 	index = key_index((unsigned char *)key, ht->size);
 
 	if (ht->array[index])
-		return (ht->array[index]->value);
-	else
-		return (NULL);
+	{
+		cursor = ht->array[index];
+		while (cursor != NULL)
+		{
+			if (strcmp(cursor->key, key) == 0)
+				return (cursor->value);
+			cursor = cursor->next;
+		}
+	}
+	return (NULL);
 }
