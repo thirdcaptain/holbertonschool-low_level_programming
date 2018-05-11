76b412cbe7e53961193a7f58c10db7e5cc110574
Isaac Wong <isaac@thirdcaptain.com>
2018-05-11 17:50:51 +0000

    Resubmit everything

--- 0x19-hash_tables/6-hash_table_delete.c
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -2,7 +2,7 @@
 
 /**
  * hash_table_delete - deletes a hash table
- * @ht - hash table to delete
+ * @ht: hash table to delete
  */
 
 void hash_table_delete(hash_table_t *ht)
@@ -16,7 +16,7 @@ void hash_table_delete(hash_table_t *ht)
 		if (ht->array[i] != NULL)
 		{
 			cursor = ht->array[i];
-			while(cursor != NULL)
+			while (cursor != NULL)
 			{
 				temp = cursor->next;
 				free(cursor);
