900c03c401591c0f72d4fd2ecefed67001d86754
Isaac Wong <isaac@thirdcaptain.com>
2018-05-11 20:35:02 +0000

    Added update logic

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -13,25 +13,55 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
 	hash_node_t *node;
+	hash_node_t *cursor;
+
+	if (key == NULL || ht == NULL || value == NULL)
+		return (0);
 
 	if (strcmp(key, "") == 0)
 		return (0);
 
-	index = key_index((unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
+	{
 		return (0);
+	}
+
 	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (0);
+	}
 	node->value = strdup(value);
+	if (node->key == NULL)
+	{
+		free(node);
+		free(node->key);
+		return (0);
+	}
 	node->next = NULL;
 
 	if (ht->array[index] != NULL)
 	{
+		cursor = ht->array[index];
+		while (cursor != NULL)
+		{
+			if (strcmp(cursor->key, key) == 0)
+			{
+				free(cursor->value);
+				cursor->value = strdup(value);
+				return (1);
+			}
+			cursor = cursor->next;
+		}
 		node->next = ht->array[index];
 		ht->array[index]->next = NULL;
 	}
-	ht->array[index] = node;
+	else
+		ht->array[index] = node;
 
 	return (1);
 }
