3a2f1061d1aaf7b436268ae0e700b92f45a8a122
Isaac Wong <isaac@thirdcaptain.com>
2018-05-11 22:00:14 +0000

    Removed NULL addition

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -59,7 +59,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht->array[index] != NULL)
 	{
 		node->next = ht->array[index];
-		ht->array[index]->next = NULL;
 	}
 	ht->array[index] = node;
 	return (1);
