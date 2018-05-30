9e8e41105d4a5128710ffc2dfddaf15712253e91
Isaac Wong <isaac@thirdcaptain.com>
2018-05-30 01:30:15 +0000

    Initial commit

---
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts a array using selection sort
+ * @array: array of ints
+ * @size: size of list
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0;
+	size_t start = 0;
+	size_t low_idx = 0;
+	int low = 0;
+	int temp = 0;
+
+	while (start < size - 1)
+	{
+		low = array[start];
+		i = 0;
+		while ((start + i) < size)
+		{
+			if (array[i + start] < low)
+			{
+				low = array[start + i];
+				low_idx = start + i;
+			}
+			i++;
+		}
+		temp = array[start];
+		array[start] = array[low_idx];
+		array[low_idx] = temp;
+		start++;
+		print_array(array, size);
+	}
+}
