82c8f82ae998ba127ffac164dd1b15c62f519bb2
Isaac Wong <isaac@thirdcaptain.com>
2018-05-30 07:19:31 +0000

    Initial commit

---
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,88 @@
+#include "sort.h"
+
+/**
+ * swap - moves linked list node
+ * @position: node after node to place node
+ * @node: the node to relocate
+ */
+void swap(listint_t *position, listint_t *node)
+{
+/*	printf("position->n:%d node->%d\n", position->n, node->n); */
+	/*node removal*/
+	if (node->next == NULL)
+	{
+		node->prev->next = NULL;
+	}
+	if (node->prev == NULL)
+	{
+		node->next->prev = NULL;
+	}
+	if (node->next != NULL && node->prev != NULL)
+	{
+		node->prev->next = node->next;
+		node->next->prev = node->prev;
+	}
+	/*node insertion*/
+	if (position->prev == NULL)
+	{
+		node->prev = NULL;
+		position->prev = node;
+		node->next = position;
+		position = node;
+		return;
+	}
+	position->prev->next = node;
+	node->prev = position->prev;
+	node->next = position;
+	position->prev = node;
+}
+
+
+/**
+ * insertion_sort_list - sorts linked list
+ * @list: list to sort
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *cursor_f;
+	listint_t *cursor_b;
+	listint_t *temp;
+	int value = 0;
+	int count = 0;
+
+	if (*list == NULL)
+		return;
+
+	cursor_f = *list;
+	while (cursor_f != NULL)
+	{
+		count++;
+		cursor_f = cursor_f->next;
+	}
+	if (count < 2)
+		return;
+
+	cursor_f = *list;
+	while (cursor_f != NULL)
+	{
+		value = cursor_f->n;
+		cursor_b = cursor_f;
+		temp = cursor_f;
+		while (cursor_b != NULL)
+		{
+			if (value < cursor_b->n)
+			{
+				if (cursor_b->prev == NULL)
+				{
+					swap(cursor_b, temp);
+					*list = temp;
+				}
+				swap(cursor_b, temp);
+				print_list(*list);
+			}
+			cursor_b = cursor_b->prev;
+		}
+		cursor_f = cursor_f->next;
+	}
+}
